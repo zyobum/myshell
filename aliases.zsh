2c15058763221714d854fde006ab24ae67783ca0
jayz <j@cbx.io>
2022-06-08 19:07:01 +0800

    switch to limactl platform

--- aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias ll='ls -alFG'
-alias u='sudo apt-get update && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get update && omz update'
-alias qrencode='qrencode -t ansiutf8 -r'
