0d5bac8304a1bd2e0f957c66e6bd8f0871eb2a0d
jayz <j@cbx.io>
2022-06-15 17:41:15 +0800

    mount devices in the mount_dev.sh after reboot

--- mount_dev.sh
+++ mount_dev.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+set -e
+
+PATH=opt/homwbrew/bin:$PATH
 DEFHOME=$HOME/.lima/default
 QMPSOCK=$DEFHOME/qmp.sock
 QMPPORT="UNIX-CONNECT:$QMPSOCK"
+
+if [ ! -w $QMPSOCK ]; then
+    "Socket not found: $QMPSOCK"
+    exit 1
+fi
+
+#Commands
+#capabilities negotiation
 CMD_NEGO='{ "execute": "qmp_capabilities" }'
 CMD_MOUNT_RD4='{"execute":"device_add", "arguments":{"driver":"usb-host", "bus":"usb-bus.0", "vendorid":"0x9ac4","productid":"0x4b8f"}}'
-#capabilities negotiation
+
+echo "Mounting devices..."
 echo -e "${CMD_NEGO}\n${CMD_MOUNT_RD4}" | socat - $QMPPORT
+
+echo "Mount finished"
 
--- shell_service.sh
+++ shell_service.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+set -e
+
 PATH=/opt/homebrew/bin:/usr/sbin:$PATH
 
 STATUS=""
+
+DEFHOME="$HOME/.lima/default"
+QMP_SOCK="$DEFHOME/qmp.sock"
+DIR="$(dirname "$0")"
+MOUNTDEV="$DIR/mount_dev.sh"
+OUT="/tmp/myshell_mount.out"
 
 check_status () {
   STATUS=$(limactl list default --json | jq .status)
@@ -18,6 +26,9 @@ while true ; do
     # restart it
     echo "Instance stopped. Starting instance"
     limactl start
+    echo "Mount devices: $MOUNTDEV"
+    date > $OUT
+    $MOUNTDEV &> $OUT
   fi
   sleep 2
 done
